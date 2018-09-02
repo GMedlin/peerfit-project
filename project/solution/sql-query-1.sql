a50dd3b9a5a29fe5d5e9393a905f3e9e0e61fc22
Garett Medlin <garettmedlin@gmail.com>
2018-09-01 19:45:45 -0500

    Changed answer to 1
    Changed answer to 1. At first I thought completed reservations where just all reservations. Now, I'm interpreting that to mean they did not cancel and they did sign/check in.

--- project/solution/sql-query-1.sql
+++ project/solution/sql-query-1.sql
@@ -3,11 +3,13 @@ with
 sq1 as (
 	select id
 	from clubready_reservations
+	where canceled = 'f' and signed_in_at is not null
 	  
 	union all
 	
 	select id
 	from mindbody_reservations
+	where isnull(canceled_at) and checked_in_at is not null
 )
 
 select count(id) as total_reserves
